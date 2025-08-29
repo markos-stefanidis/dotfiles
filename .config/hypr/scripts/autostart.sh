a493e66cf855b3f95a3dbc8a20c7653e7474f8bf
markos-stefanidis <markos.stefanides@gmail.com>
2025-08-29 19:33:20 +0300

    Update hyprland config

--- .config/hypr/scripts/autostart.sh
+++ .config/hypr/scripts/autostart.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-
 killall swabg
 killall eww
 
@@ -18,8 +17,8 @@ systemctl --user start hyprpolkitagent &
 dbus-update-activation-environment &
 nm-applet &
 
-xrandr --output HDMI-0 --mode 1280x1024 --noprimary --left-of DP-0 &
-xrandr --output DP-0 --primary &
+xrandr --output DP-1 --primary &
+xrandr --output HDMI-A-1 --mode 1280x1024 --noprimary --left-of DP-0 &
 
 eww open bar0; eww open bar1
 envsubst < $HOME/.config/wofi/style_template.css > $HOME/.config/wofi/style.css
